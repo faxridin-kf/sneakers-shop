65c674a5145ed975bc9f04d64dbdd140dfa9f193
pro <faxridinkf@gmail.com>
2022-02-09 16:00:04 +0500

    day 4 add functions visible cart and plus btn

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import sneaker from './assets/sneakers/1.jpg'
+import axios from 'axios';
 import './App.css';
 import { Card, Drawer, Header, Search } from './components';
-const arr = [
-  { name: 'hello', price: 3093 },
-  { name: 'sneakers', price: 3093 },
-  { name: 'bro', price: 3093 },
-  { name: 'koca style', price: 3093 },
-]
+
 function App() {
+  const [cartItems, setCartItems] = React.useState([])
+  const [cartOpen, setCartOpen] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    axios.get('https://62036da04d21c200170b9d05.mockapi.io/items').then(({ data }) => {
+      setItems(data)
+    })
+  }, []);
+  const addToCart = (obj) => {
+    setCartItems([...cartItems, obj])
+    console.log(cartItems);
+    // setCartItems(prev => [...prev, obj])
+  };
   return (
     <>
       <div className='wrapper clear'>
-        <Drawer />
-        <Header />
+        {cartOpen && <Drawer items={cartItems} onClose={() => setCartOpen(false)} />}
+        <Header onClickCart={() => setCartOpen(true)} />
         <div className="content p-40">
           <Search />
         </div>
-        <div className='sneakers d-flex'>
-          {arr.map((obj, index) => <Card
-            key={index + obj}
-            title={obj.name}
-            price={obj.price}
-            imgUrl={sneaker}
-            onClick={() => console.log(obj)}
+        <div className='sneakers d-flex flex-wrap'>
+          {items.map((item, index) =>
+            <Card
+              key={index + item}
+              title={item.title}
+              price={item.price}
+              imgUrl={item.imageUrl}
+              onFavoriteClick={null}
+              onPlusClick={(obj) => addToCart(obj)}
           />)}
         </div>
       </div>
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,42 +1,33 @@
 import React from 'react';
-import sneaker from '../assets/sneakers/1.jpg'
-
-export default function Drawer() {
+export default function Drawer({ onClose, items = [] }) {
   return (
     <>
-      <div style={{ display: 'none' }} className='overlay'>
+      <div className='overlay'>
         <div className='drawer'>
           <div className='goodBlock'>
             <div>
               <h2 className='d-flex align-center justify-between'>Order Cart
-                <svg className='cu-p' width='32' height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg onClick={onClose} className='cu-p' width='32' height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB" />
                   <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5" />
                 </svg> </h2>
             </div>
             <div className='goodsList'>
-              <div className='cartItem d-flex align-center mb-20'>
-                <img className='mr-20' width={70} height={70} src={sneaker} alt='snekers' />
-                <div className='mr-20'>
-                  <p className='mb-5'>Man's sneakers Nike Air Max 270</p>
-                  <b>123 233 rub.</b>
-                </div>
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB" />
-                  <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5" />
-                </svg>
-              </div>
-              <div className='cartItem d-flex align-center mb-20'>
-                <img className='mr-20' width={70} height={70} src={sneaker} alt='snekers' />
-                <div className='mr-20'>
-                  <p className='mb-5'>Man's sneakers Nike Air Max 270</p>
-                  <b>123 233 rub.</b>
-                </div>
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB" />
-                  <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5" />
-                </svg>
-              </div>
+              {
+                items.map((item) => {
+                  <div className='cartItem d-flex align-center mb-20'>
+                    <img className='mr-20' width={70} height={70} src={item.imageUrl} alt='snekers' />
+                    <div className='mr-20'>
+                      <p className='mb-5'>{item.title}</p>
+                      <b>{item.price} rub.</b>
+                    </div>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB" />
+                      <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5" />
+                    </svg>
+                  </div>
+                })
+              }
             </div>
           </div>
           <div className='items'>
